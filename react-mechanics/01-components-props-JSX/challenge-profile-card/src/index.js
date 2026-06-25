333cfbb13d5db2365ffde65293b95d18368f1062
Angelika Sowka <sowkaangelika@gmail.com>
2026-06-24 20:01:41 -0400

    fundamentals of events and state

--- react-mechanics/01-components-props-JSX/challenge-profile-card/src/index.js
+++ react-mechanics/01-components-props-JSX/challenge-profile-card/src/index.js
@@ -2,6 +2,34 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 
+const skills = [
+  {
+    skill: "HTML + CSS ",
+    level: "begginer",
+    color: "#f7c1b5",
+  },
+  {
+    skill: "JavaScript ",
+    level: "advanced",
+    color: "#b5f5c1",
+  },
+  {
+    skill: "Web Design ",
+    level: "advanced",
+    color: "#c5d0ff",
+  },
+  {
+    skill: "Git and GitHub ",
+    level: "intermediate",
+    color: "#ecf0b7",
+  },
+  {
+    skill: "React ",
+    level: "advanced",
+    color: "#f3b5ef",
+  },
+];
+
 function App() {
   return (
     <div className="container">
@@ -34,17 +62,24 @@ function Image() {
 function ToolsList() {
   return (
     <div className="tools-container">
-      <ToolTag toolName="HTML + CSS 💪🏻" />
-      <ToolTag toolName="JavaScript💪🏻" />
-      <ToolTag toolName="Web Design 💪🏻" />
-      <ToolTag toolName="Git and GitHub 👌🏻" />
-      <ToolTag toolName="React 👍🏻" />
+      {skills.map((skill) => (
+        <ToolTag skill={skill.skill} color={skill.color} level={skill.level} />
+      ))}
     </div>
   );
 }
 
-function ToolTag(props) {
-  return <p className="tool">{props.toolName}</p>;
+function ToolTag({ skill, color, level }) {
+  return (
+    <div className="tool" style={{ backgroundColor: color }}>
+      <span>{skill}</span>
+      <span>
+        {level === "begginer" && "👶"}
+        {level === "advanced" && "👌🏻"}
+        {level === "intermediate" && "👍🏻"}
+      </span>
+    </div>
+  );
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
